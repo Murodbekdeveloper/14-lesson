e5f612403d6d8505b6a5228033a765de23631ed8
Murodbek Developer <164814926+Murodbekdeveloper@users.noreply.github.com>
2026-09-21 19:28:12 +0500

    edit token

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): any {
-    console.log('Server run with Port');
-    return true;
+    console.log('Server run with Port,');
+    return false,
   }
 }
